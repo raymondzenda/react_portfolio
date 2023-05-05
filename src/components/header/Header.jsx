b11b6b45436dc2bf4448f7635301f9e10c224bbc
Raymond Zenda <raymondzenda14@gmail.com>
2023-05-05 14:11:41 +0200

    css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header_container">
+        <h5>Hello i'm </h5>
+        <h1>Raymond</h1>
+        <h5 className="text-light">Software Engineer</h5>
+      </div>
+    </header>
   )
 }
 
